c6ee6803a6a298e822d44d5ed26a61e9c533e286
alhakammar2-oss <alhakammar2@gmail.com>
2026-01-23 02:59:08 +0300

    Add currency conversion functionality in script.js

---
+++ script.js
@@ -0,0 +1,9 @@
+const rates = { yer:1, sar:150, usd:550 };
+document.getElementById('convertBtn').onclick = () => {
+  let a = amount.value;
+  if(!a){
+    currencyResult.textContent = 'أدخل مبلغ';
+    return;
+  }
+  // باقي الكود
+};
